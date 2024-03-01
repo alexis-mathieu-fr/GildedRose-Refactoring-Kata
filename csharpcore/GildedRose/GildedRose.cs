b53315924c2f9ee42b6bb0fff181cd585bf1f810
Alexis MATHIEU <alexis.mathieu.p@citeo.com>
2024-03-01 08:37:33 +0100

    Idées du premier coding dojo

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -87,4 +87,91 @@ public class GildedRose
     {
         if (item.Quality > 0) item.Quality -= value;
     }
+
+    public void UpdateBackstagePass(Item item)
+    {
+        if (item.Quality < 50)
+        {
+            increment(item, 1);
+
+            if (item.Name == BackstagePassesToATafkal80EtcConcert && item.SellIn < 11 && item.Quality < 50)
+            {
+                increment(item, 1);
+                if (item.SellIn < 6)
+                {
+                    increment(item, 1);
+                }
+            }
+        }
+
+        if (item.Name != SulfurasHandOfRagnaros)
+        {
+            item.SellIn = item.SellIn - 1;
+        }
+
+        if (item.SellIn >= 0) return;
+
+        item.Quality = 0;
+    }
+
+    public void UpdateAgedBrie(Item item)
+    {
+        if (item.Name != AgedBrie && item.Name != BackstagePassesToATafkal80EtcConcert)
+        {
+            if (item.Quality > 0 && item.Name != SulfurasHandOfRagnaros)
+            {
+                decrement(item, 1);
+            }
+        }
+        else
+        {
+            if (item.Quality < 50)
+            {
+                increment(item, 1);
+
+                if (item.Name == BackstagePassesToATafkal80EtcConcert && item.SellIn < 11 && item.Quality < 50)
+                {
+                    increment(item, 1);
+                    if (item.SellIn < 6)
+                    {
+                        increment(item, 1);
+                    }
+                }
+            }
+        }
+
+        if (item.Name != SulfurasHandOfRagnaros)
+        {
+            item.SellIn = item.SellIn - 1;
+        }
+
+        if (item.SellIn < 0)
+        {
+            if (item.Name != AgedBrie)
+            {
+                if (item.Name != BackstagePassesToATafkal80EtcConcert)
+                {
+                    if (item.Quality > 0)
+                    {
+                        if (item.Name != SulfurasHandOfRagnaros)
+                        {
+                            decrement(item, 1);
+                        }
+                    }
+                }
+                else
+                {
+                    item.Quality = 0;
+                }
+            }
+            else
+            {
+                if (item.Quality < 50)
+                {
+                    increment(item, 1);
+                }
+            }
+        }
+    }
+    }
 }
